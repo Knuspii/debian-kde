2a9c1a03a4f7ec6dc85a031216ece5addffb566b
Knuspii <220954979+Knuspii@users.noreply.github.com>
2026-02-12 15:46:12 +0100

    Add autostart directory and update prompts

--- debian-kde-install.sh
+++ debian-kde-install.sh
@@ -54,10 +54,13 @@ HOME_DIR=$(eval echo "~$USERNAME")
 # Create needed directories
 mkdir -p "$HOME_DIR/.config/conky"
 mkdir -p "$HOME_DIR/.config/wallpaper"
+mkdir -p "$HOME_DIR/.config/autostart"
 # Download dotfiles
 wget -qO "$HOME_DIR/.bashrc" ${url}/config/bashrc
 wget -qO "$HOME_DIR/.bash_profile" ${url}/config/bashprofile
 # Download configs
+wget -qO "$HOME_DIR/.config/autostart/conky.desktop" ${url}/config/autostart/conky.desktop
+wget -qO "$HOME_DIR/.config/autostart/terminal.desktopg" ${url}/config/autostart/terminal.desktop
 wget -qO "$HOME_DIR/.config/wallpaper/wallpaper.png" ${url}/config/wallpaper/wallpaper.png
 wget -qO "$HOME_DIR/.config/conky/conky.config" ${url}/config/conky/conky.config
 # Fix ownership
@@ -81,7 +84,7 @@ flatpak update --assumeyes
 
 # Reboot at the end :)
 echo Installation finished!
-read -p "Do you want to continue? [y/n] " answer
+read -p "Do you want to reboot? [y/n] " answer
 case "$answer" in
     [Yy]* ) echo "OK, continuing...";;
     [Nn]* ) echo "Aborted"; exit 1;;
