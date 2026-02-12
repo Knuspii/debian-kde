74ac87bda4bce0f6ec9fa3e13e39c960f124f8c2
Knuspii <220954979+Knuspii@users.noreply.github.com>
2026-02-12 18:18:58 +0100

    Add KDE configuration files to installation script

--- debian-kde-install.sh
+++ debian-kde-install.sh
@@ -59,6 +59,16 @@ mkdir -p "$HOME_DIR/.config/autostart"
 wget -qO "$HOME_DIR/.bashrc" ${url}/config/bashrc
 wget -qO "$HOME_DIR/.bash_profile" ${url}/config/bashprofile
 # Download configs
+# KDE stuff
+wget -qO "$HOME_DIR/.config/kdeglobals" ${url}/config/kdeglobals
+wget -qO "$HOME_DIR/.config/backup-config.sh" ${url}/config/backup-config.sh
+wget -qO "$HOME_DIR/.config/kglobalshortcutsrc" ${url}/config/kglobalshortcutsrc
+wget -qO "$HOME_DIR/.config/kscreenlockerrc" ${url}/config/kscreenlockerrc
+wget -qO "$HOME_DIR/.config/kwinrc" ${url}/config/kwinrc
+wget -qO "$HOME_DIR/.config/plasma-org.kde.plasma.desktop-appletsrc" ${url}/config/plasma-org.kde.plasma.desktop-appletsrc
+wget -qO "$HOME_DIR/.config/plasmarc" ${url}/config/plasmarc
+# Special stuff
+wget -qO "$HOME_DIR/.config/backup-config.sh" ${url}/config/backup-config.sh
 wget -qO "$HOME_DIR/.config/autostart/conky.desktop" ${url}/config/autostart/conky.desktop
 wget -qO "$HOME_DIR/.config/autostart/terminal.desktopg" ${url}/config/autostart/terminal.desktop
 wget -qO "$HOME_DIR/.config/wallpaper/wallpaper.png" ${url}/config/wallpaper/wallpaper.png
@@ -74,6 +84,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install --assumeyes flathub \
 org.mozilla.firefox \
 org.libreoffice.LibreOffice \
+org.keepassxc.KeePassXC \
 com.discordapp.Discord \
 com.visualstudio.code \
 org.gimp.GIMP \
