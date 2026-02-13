1dc61ffb8e0f6655b4bc43bd993ff29ee75b34c7
Knuspii <220954979+Knuspii@users.noreply.github.com>
2026-02-13 09:08:17 +0100

    Refactor Debian KDE install script
    Updated script to focus on installing Debian and KDE, and removed extra comments and packages.

---
+++ debian-only-kde-install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Script to install Debian & KDE
+# Made by Knuspii
+
+url="https://raw.githubusercontent.com/knuspii/debian-kde/main"
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root."
+  exit 1
+fi
+
+# Update packages and install KDE
+apt update && apt upgrade -y
+apt install kde-plasma-desktop plasma-nm konsole sddm -y
+systemctl enable sddm
+
+# Remove bloat
+apt purge -y \
+plasma-discover \
+kdeconnect \
+
+# Install essentials with apt
+apt install -y \
+ssh \
+curl \
+wget \
+unzip \
+htop \
+tree \
+whois \
+
+exit 0
